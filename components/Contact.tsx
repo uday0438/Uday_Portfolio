420562f4f1d72f8e09183466e140e432615a7494
Your Name <you@example.com>
2026-05-15 12:55:05 +0530

    Fix LinkedIn profile URL with absolute path

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -39,7 +39,7 @@ const Contact: React.FC = () => {
 
   const socialLinks = [
     { Icon: Github, href: 'https://github.com/uday0438', label: 'GitHub' },
-    { Icon: Linkedin, href: 'www.linkedin.com/in/kalle-uday-bhaskar', label: 'LinkedIn' },
+    { Icon: Linkedin, href: 'https://www.linkedin.com/in/kalle-uday-bhaskar/', label: 'LinkedIn' },
     { Icon: Instagram, href: 'https://instagram.com/', label: 'Instagram' },
     { Icon: Send, href: 'https://t.me/', label: 'Telegram' },
   ];
